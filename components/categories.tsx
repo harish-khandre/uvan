7def19ce286942b678d705ea9bfbe7b4f74a2d2c
haris-khandre <harishkhandre2901@gmail.com>
2024-04-06 21:01:04 +0530

    changed category

--- components/categories.tsx
+++ components/categories.tsx
@@ -20,13 +20,13 @@ async function fetchCategories() {
 export default async function Categories() {
   const categories = await fetchCategories();
   return (
-    <div className="flex justify-start lg:pl-16 pt-10 items-center h-full w-[70%]">
+    <div className="flex justify-start lg:pl-16 py-5 items-center h-full w-[70%]">
       <p className="p-2 text-gray-500">Categories</p>
       <p className="p-2"></p>
       <div className="border-b flex justify-center items-center gap-x-4 p-2 text-sm">
         {categories.map((category: string) => (
           <Link
-            className="border-l-zinc-200 border-l-2 pl-2"
+            className="border-l-zinc-200 border-l-2 px-2 text-base text-gray-600 hover:text-gray-950"
             href={`/category/${category}`}
             key={category}
           >
