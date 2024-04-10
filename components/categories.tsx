7ad79ae476255c9953f6a339623e2acd21e1f565
haris-khandre <harishkhandre2901@gmail.com>
2024-04-10 10:00:21 +0530

    category scroll

--- components/categories.tsx
+++ components/categories.tsx
@@ -20,10 +20,10 @@ async function fetchCategories() {
 export default async function Categories() {
   const categories = await fetchCategories();
   return (
-    <div className="flex justify-start lg:pl-16 py-5 items-center h-full w-[70%]">
+    <div className="flex justify-start lg:pl-16 py-5 items-center h-full  lg:w-[70%] scrollie">
       <p className="p-2 text-gray-500">Categories</p>
       <p className="p-2"></p>
-      <div className="border-b flex justify-center items-center gap-x-4 p-2 text-sm">
+      <div className=" flex justify-start items-center gap-x-4 p-2 overflow-x-scroll scrollie text-sm">
         {categories.map((category: string) => (
           <Link
             className="border-l-zinc-200 border-l-2 px-2 text-base text-gray-600 hover:text-gray-950"
