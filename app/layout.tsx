d3c0a4b80dca89bc4d89e5d96a06a2646107989d
haris-khandre <harishkhandre2901@gmail.com>
2024-05-22 16:21:25 +0530

    added script tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Merriweather } from "next/font/google";
 import "./globals.css";
 import { Navbar } from "@/components/navbar";
 import Footer from "@/components/footer";
-import Head from "next/head";
+import Script from "next/script";
 
 const merri = Merriweather({ weight: "400", subsets: ["latin"] });
 
@@ -24,13 +24,13 @@ export default function RootLayout({
   return (
     <>
       <html lang="en">
-        <Head>
-          <script
+        <head>
+          <Script
             async
             src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2173260918120839"
             crossOrigin="anonymous"
-          ></script>
-        </Head>
+          />
+        </head>
         <body className={merri.className}>
           <Navbar />
           {children}
