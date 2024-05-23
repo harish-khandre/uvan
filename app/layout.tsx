f091a3e6ca23c646200e5135421ac674ec42c477
haris-khandre <harishkhandre2901@gmail.com>
2024-05-23 13:48:56 +0530

    removed funding choice

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,9 +30,6 @@ export default function RootLayout({
             src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2173260918120839"
             crossOrigin="anonymous"
           />
-          <Script 
-         async src="https://fundingchoicesmessages.google.com/i/pub-2173260918120839?ers=1" nonce="ZOV-PhkPMVOV67FUzxWeAg" />
-          <Script nonce="ZOV-PhkPMVOV67FUzxWeAg">(function() {function signalGooglefcPresent() {if (!window.frames['googlefcPresent']) {if (document.body) {const iframe = document.createElement('iframe'); iframe.style = 'width: 0; height: 0; border: none; z-index: -1000; left: -1000px; top: -1000px;'; iframe.style.display = 'none'; iframe.name = 'googlefcPresent'; document.body.appendChild(iframe);} else {setTimeout(signalGooglefcPresent, 0);}}}signalGooglefcPresent();})() />
         </head>
         <body className={merri.className}>
           <Navbar />
