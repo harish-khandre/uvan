c8fb10c0fa0694b89104a571bc96162ae469667d
haris-khandre <harishkhandre2901@gmail.com>
2024-07-11 17:42:04 +0530

    add contact us

---
+++ app/contact/page.tsx
@@ -0,0 +1,11 @@
+export default function ContactUs() {
+  return (
+    <div className="flex flex-col justify-center items-center h-screen gap-4">
+      <h1 className="text-2xl">Contact Us</h1>
+      <p text-lg>
+        If you have any queries regarding Site, Advertisement, and any other
+        issue, please feel free to contact me at uvantech1801@gmail.com
+      </p>
+    </div>
+  );
+}
--- components/footer.tsx
+++ components/footer.tsx
@@ -18,20 +18,25 @@ export default function Footer() {
     {
       idx: 3,
       href: "/privacy",
-      name: "Privacy",
+      name: "Privacy Policy",
     },
     {
       idx: 4,
       href: "/about",
       name: "About us",
     },
+    {
+      idx: 5,
+      href: "/contact",
+      name: "Contact us",
+    },
   ];
   return (
     <footer className="pt-10">
       <div className="max-w-screen-xl mx-auto px-4 text-gray-600 md:px-8">
-        <div className="mt-10 py-10 border-t items-center justify-between sm:flex">
-          <p>© 2024 UvanTech Inc. All rights reserved.</p>
-          <ul>
+        <div className="mt-10 py-10 border-t flex items-center justify-between ">
+          © 2024 UvanTech Inc. All rights reserved.
+          <ul className="flex gap-4">
             {footerNavs.map((item) => (
               <li
                 key={item.idx}
